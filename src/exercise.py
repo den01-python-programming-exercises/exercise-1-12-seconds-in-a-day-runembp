c68e294a6979e806cc4070f1a3db61eb45d04836
Rune Petersen <58623004+runembp@users.noreply.github.com>
2021-08-22 17:55:47 +0000

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,7 @@
 def main():
     #write your code below this line
-
+    number = int(input("How many days would you like to convert to seconds?"))
+    sum = number * 60 * 60 * 24
+    print(sum)
 if __name__ == '__main__':
     main()
